0fe5a4be26ceeebc7ffe9a6834fcf0ea7cd227fe
Brian K <50558629+bralkor@users.noreply.github.com>
2023-05-01 18:17:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('woohoo')
+    print_hi('yay')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
